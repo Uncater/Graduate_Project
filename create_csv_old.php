b015e3370c0121fbcd5fbbe38b25772230517695
Uncater <leonid.neugodnikov@gmail.com>
2015-12-06 23:09:33 +0200

    new functionality for csv creation

---
+++ create_csv_old.php
@@ -0,0 +1,9 @@
+<?php
+$handler = fopen("temp/csv_old.csv" , "w");
+$urls = $_POST['url'];
+$headers = $_POST['H1'];
+$result = $urls . PHP_EOL . $headers;
+if(fwrite($handler, $result))
+    echo "File is created successfully";
+else echo "Something went terribly wrong";
+fclose($handler);
